419d019ec9d78cdfb2f668b1bcc6a6b1569cac4a
Sagar Modi <sm185435@ncr.com>
2019-03-11 13:19:14 +0530

    - Test Commit

--- app/src/main/java/com/sagar/testci/SwipeLayout.java
+++ app/src/main/java/com/sagar/testci/SwipeLayout.java
@@ -10,7 +10,6 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
